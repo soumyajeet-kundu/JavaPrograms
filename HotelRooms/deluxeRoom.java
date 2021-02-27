8660991da2836c306a812c8c7d9b7d08013a0a18
Soumyajeet Kundu <78849208+soumyajeet-kundu@users.noreply.github.com>
2021-02-27 14:02:26 +0530

    Update deluxeRoom.java

--- HotelRooms/deluxeRoom.java
+++ HotelRooms/deluxeRoom.java
@@ -4,24 +4,30 @@ public class deluxeRoom extends HotelRoom {
     public deluxeRoom(String hotelName, int numberOfSqFeet, boolean hasTv, boolean hasWifi){
         super(hotelName, numberOfSqFeet, hasTv, hasWifi);
 
-            if(hasWifi == true);
+            if(hasWifi == false)
+            {
+                rateCalculatorDR();
+            }
+            else if(hasWifi == true)
             {
                 rate1();
             }
-            rateCalculatorDR();
         }
+        
 
         public int rateCalculatorDR() {
             totalDR = ratePerSqFeet * getNumberOfSqFeet();
             System.out.println("Total rate : " + totalDR);
             return 0;
         }
-
         public int rate1(){
             ratePerSqFeet = ratePerSqFeet + 2;
             totalDR = ratePerSqFeet * getNumberOfSqFeet();
+            System.out.println("Total rate : " + totalDR);
             return 0;
         }
+
+        
         
     }
 
