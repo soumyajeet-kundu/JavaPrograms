300d135b60dc84d040743cf2691e5e034a785413
Soumyajeet Kundu <78849208+soumyajeet-kundu@users.noreply.github.com>
2021-02-27 15:53:44 +0530

    Update ExcHand1.java

--- ExceptionHandling/ExcHand1.java
+++ ExceptionHandling/ExcHand1.java
@@ -6,17 +6,22 @@ import javax.swing.text.Style;
 public class ExcHand1 {
     public static void main(String[] args) throws Exception{
 
+        try{
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         System.out.println("enter the total run scored : ");
         int run = Integer.parseInt(br.readLine());
         System.out.println("enter the total overs faced : ");
         int ovr = Integer.parseInt(br.readLine());
-        try{
-            float rr = (run/ovr);
-            System.out.println("Current run rate : " + rr);
-            }
+    
+        float rr = (run/ovr);
+        System.out.println("Current run rate : " + rr);
+        }
         catch(ArithmeticException e){
             System.out.println("ArithmaticException -- can't divide by zero");
+        }
+        catch(NumberFormatException e1){
+            System.out.println("NumberFormatException -- you should enter a number");
+            System.exit(0);
         }
 
 }
