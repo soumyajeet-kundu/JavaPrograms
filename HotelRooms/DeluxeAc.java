9f72cd1ee51b8dab5e60db6165bb71bc95ed9ce1
Soumyajeet Kundu <78849208+soumyajeet-kundu@users.noreply.github.com>
2021-02-27 14:00:50 +0530

    Update DeluxeAc.java

--- HotelRooms/DeluxeAc.java
+++ HotelRooms/DeluxeAc.java
@@ -2,7 +2,20 @@ public class DeluxeAc extends HotelRoom {
     int totalDR, ratePerSqFeet1=12;
     public DeluxeAc(String hotelName, int numberOfSqFeet, boolean hasTv, boolean hasWifi){
         super(hotelName, numberOfSqFeet, hasTv, hasWifi);
+            if(hasWifi == false)
+            {
+                rateCalculatorDR();
+            }
+            else if(hasWifi == true)
+            {
+                rate1();
+            }
+        }
 
+        public int rate1(){
+            ratePerSqFeet1 = ratePerSqFeet1 + 2;
+            totalDR = ratePerSqFeet1 * getNumberOfSqFeet();
+            return 0;
         }
 
         public int rateCalculatorDR() {
