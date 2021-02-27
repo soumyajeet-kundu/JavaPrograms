23ffb59ceaea16a9f3f74b01ed783005fd7e06e2
Soumyajeet Kundu <78849208+soumyajeet-kundu@users.noreply.github.com>
2021-02-27 14:03:00 +0530

    Update suiteAc.java

--- HotelRooms/suiteAc.java
+++ HotelRooms/suiteAc.java
@@ -2,7 +2,20 @@ public class suiteAc extends HotelRoom{
     int totalDR, ratePerSqFeet2=15;
     public suiteAc(String hotelName, int numberOfSqFeet, boolean hasTv, boolean hasWifi){
         super(hotelName, numberOfSqFeet, hasTv, hasWifi);
+            if(hasWifi == false)
+            {
+                rateCalculatorDR();
+            }
+            else if(hasWifi == true)
+            {
+                rate1();
+            }   
+        }
 
+        public int rate1(){
+            ratePerSqFeet2 = ratePerSqFeet2 + 2;
+            totalDR = ratePerSqFeet2 * getNumberOfSqFeet();
+            return 0;
         }
 
         public int rateCalculatorDR() {
