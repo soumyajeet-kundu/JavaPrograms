e126d6e5ff9a338206cf0113327c362eb7a9bbf5
Soumyajeet Kundu <78849208+soumyajeet-kundu@users.noreply.github.com>
2021-03-01 14:55:50 +0530

    Add files via upload

---
+++ Activity9/Ranking.java
@@ -0,0 +1,37 @@
+import java.util.Comparator;
+
+public class Ranking {
+	private int score;
+    private String name;
+ 
+    // Constructor
+    public Ranking(int score, String name)
+    {
+        this.score = score;
+        this.name = name;
+
+    }
+ 
+    // Used to print student details in main()
+    public String toString()
+    {
+        return this.score + " " + this.name;
+    }
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+}
+ 
+class SortByScore implements Comparator<Ranking>
+{
+ 
+    public int compare(Ranking a, Ranking b)
+    {
+        return a.getScore() - b.getScore();
+    }
+}
