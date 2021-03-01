3c2147f610367650653900ef484612eeede8efc8
Soumyajeet Kundu <78849208+soumyajeet-kundu@users.noreply.github.com>
2021-03-01 14:57:50 +0530

    Update Ranking.java

--- Activity9/Ranking.java
+++ Activity9/Ranking.java
@@ -32,6 +32,6 @@ class SortByScore implements Comparator<Ranking>
  
     public int compare(Ranking a, Ranking b)
     {
-        return a.getScore() - b.getScore();
+        return b.getScore() - a.getScore();
     }
 }
